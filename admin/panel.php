331b98149ff07343dd7f42394cbcc2bbcf60ae54
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2023-03-03 14:15:48 +0100

    Update panel.php

--- admin/panel.php
+++ admin/panel.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['isAdmin']))
   header('Location: ping.php');
 if(@$_POST['username'] == "admin" && @$_POST['password'] == "ILoveYou!")
 {
-  if(@$_POST['isAdmin'] == "false"))
+  if(@$_POST['isAdmin'] == "false")
   {
     
     $_SESSION['isAdmin'] = false;
