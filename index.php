b68240a5143d9c938e428854515dcada53d28782
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2023-03-03 13:35:43 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Ping WebApp 1.0</title>
+</head>
+<body>
+<center>
+<h1>Ping WebApp 1.0</h1>
+<hr>
+Sorry, unfortunately the site is under maintenance. Try again later.<p>
+By <i>Ping WeApp Administrator<i>
+</body>
+</html>
