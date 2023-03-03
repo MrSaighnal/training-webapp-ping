f14e4e698a9002c8dda50bc2d06fa593701b9843
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2023-03-03 13:43:33 +0100

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Admin Panel</title>
+  </head>
+  <body>
+    <h1>Admin Panel - Restricted Area</h1>
+    <form method=POST action=panel.php>
+      Username:  <input type=text name=username>
+      Password:  <input type=password name=password>
+      <input type=hidden name=isAdmin value=false>
+      <input type=submit value=send>
+    </form>
+  </body>
+</html>
