7789a35044cc418b2e5095b6c2a54c49d40fd8c3
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2023-03-03 13:48:54 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -10,5 +10,6 @@
       <input type=hidden name=isAdmin value=false>
       <input type=submit value=send>
     </form>
+    <?php echo "<font color=red>".@$_GET['print']."</font>"; ?>
   </body>
 </html>
